0341691a8ffef9977f9265dd7b0f1a8ac5468e87
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-01-08 19:54:53 -0600

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Link from 'next/link';
+
+import Logo from '@/components/Logo';
+
+import DecorativeSwoops from './DecorativeSwoops';
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <div className={styles.wrapper}>
+      <DecorativeSwoops />
+      <div className={styles.content}>
+        <div>
+          <Logo mobileAlignment="center" />
+          {/*
+            NOTE: If you'd like to build your blog on top
+            of this code, the license requires that you leave
+            this paragraph untouched. Check out LICENSE.md
+            for more information.
+          */}
+          <p className={styles.attribution}>
+            Blog template created by{' '}
+            <a href="">
+              
+            </a>
+            . Check out{' '}
+            <a href="">
+            
+            </a>{' '}
+          
+          </p>
+        </div>
+        <nav>
+          <h2 className={styles.linkHeading}>Links</h2>
+          <ul className={styles.linkList}>
+            <li>
+              <Link href="/rss">RSS feed</Link>
+            </li>
+            <li>
+              <Link href="/todo">Terms of Use</Link>
+            </li>
+            <li>
+              <Link href="/todo">Privacy Policy</Link>
+            </li>
+            <li>
+              <a href="">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
